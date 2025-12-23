de66045965356456073ad2b4e5579f1138c89360
nahunoise <nahunoise@gmail.com>
2025-12-23 13:03:00 +0100

    Traduccion ingles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+        prefixDefaultLocale: false // 'false' si quieres que el español no tenga /es/
+    }
+  }
+});
